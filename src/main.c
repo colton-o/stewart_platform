4436fc4e84676124178e0e709ce2f279caa3fd11
Colton-O <colton.o@hotmail.com>
2023-06-06 12:11:25 +0800

    added uart system

--- src/main.c
+++ src/main.c
@@ -2,24 +2,29 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/pwm.h>
+#include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
 // setting period of motor to 50htz
 #define PERIOD (USEC_PER_SEC / 50U)
-#define STEP 100
+#define STEP 25
 #define MINPULSE 1000
 #define MAXPULSE 2000
-#define MIDPULSE 2000
+#define MIDPULSE 1500
 #define SLEEP_TIME_S 1
 #define SERVO_NUM 6
+#define RECEIVE_TIMEOUT 100
+#define RX_BUFFER_SIZE 10
 
+static uint8_t rx_buffer[RX_BUFFER_SIZE] = {0};
+static uint8_t tx_buffer[] = "we doin uart \n\r";
+const struct device *uart = DEVICE_DT_GET(DT_NODELABEL(uart0));
 typedef struct {
   const struct pwm_dt_spec name;
   uint32_t pulse;
   int direction;
 } servo;
-
 servo servos[SERVO_NUM] = {{PWM_DT_SPEC_GET(DT_ALIAS(alpha)), MINPULSE, 1},
                            {PWM_DT_SPEC_GET(DT_ALIAS(beta)), MINPULSE, 1},
                            {PWM_DT_SPEC_GET(DT_ALIAS(gamma)), MINPULSE, 1},
@@ -34,29 +39,48 @@ void set_Servos(servo *servos) {
             PWM_USEC(servos[i].pulse), 0);
   }
 }
+
 uint32_t angle_to_pulse(uint8_t angle) {
   float angle_pct = ((float)angle / 180.0f);
   uint32_t pulse = MINPULSE + (angle_pct * (MAXPULSE - MINPULSE));
   return pulse;
 }
 
+static void callback_uart(const struct device *dev, struct uart_event *evt,
+                          void *user_data) {
+  switch (evt->type) {
+  case UART_RX_RDY:
+    if ((evt->data.rx.len) == 1) {
+      switch (evt->data.rx.buf[evt->data.rx.offset]) {
+      case 'q':
+        for (int i = 0; i < 6; i++)
+          servos[i].pulse += STEP;
+        set_Servos(servos);
+        break;
+      case 'w':
+        for (int i = 0; i < 6; i++)
+          servos[i].pulse -= STEP;
+        set_Servos(servos);
+        break;
+      }
+    }
+    break;
+  case UART_RX_DISABLED:
+    uart_rx_enable(dev, rx_buffer, sizeof rx_buffer, RECEIVE_TIMEOUT);
+    break;
+  default:
+    break;
+  }
+}
 void main(void) {
-  int step = 500;
-  int dir = 1;
   printk("lets begin \n");
   set_Servos(servos);
-  while (1) {
-    printk("PWM device cycle\n");
-    if (servos[0].pulse == MAXPULSE)
-      dir = -1;
-    if (servos[0].pulse == MINPULSE)
-      dir = 1;
+  uart_callback_set(uart, callback_uart, NULL);
+  uart_tx(uart, tx_buffer, sizeof(tx_buffer), SYS_FOREVER_MS);
+  uart_rx_enable(uart, rx_buffer, sizeof(rx_buffer), RECEIVE_TIMEOUT);
 
-    for (int i = 0; i < 6; i++) {
-      servos[i].pulse += step * dir;
-    }
-
-    k_sleep(K_SECONDS(1));
-    set_Servos(servos);
-  }
+  if (!device_is_ready(uart))
+    printk("no uart device");
+  else
+    printk("we got uart");
 }
